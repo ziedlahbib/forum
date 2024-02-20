2f4c4d7a4c19d61be166e130316ad616d8a7433c
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2024-02-20 18:52:37 +0100

    update vote question

--- src/main/java/com/pidev/backend/Controller/VoteController.java
+++ src/main/java/com/pidev/backend/Controller/VoteController.java
@@ -1,5 +1,6 @@
 package com.pidev.backend.Controller;
 
+import com.pidev.backend.Entity.Question;
 import com.pidev.backend.Entity.Reponse;
 import com.pidev.backend.Entity.Vote;
 import com.pidev.backend.Service.VoteServiceImpl;
@@ -15,7 +16,7 @@ public class VoteController {
 
     @PostMapping("/add-delete-vote/{id-use}/{id-question}")
     @ResponseBody
-    public void ajoutdeletevote(@RequestBody Vote p, @PathVariable("id-question") String idq, @PathVariable("id-use") String idu) {
+    public void ajoutdeletevote(@RequestBody Question p, @PathVariable("id-question") String idq, @PathVariable("id-use") String idu) {
          voteserv.ajoutdeletevote(p,idu,idq);
 
     }
